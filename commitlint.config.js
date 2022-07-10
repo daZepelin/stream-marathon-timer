b4155cd6daab5bb67d062932d6f161dc222754bc
Vytautas Serelis <vytutas2@gmail.com>
2022-07-10 11:40:11 +0300

    chore: initial

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'chore',
+          'build',
+          'ci',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'tweak',
+        ],
+      ],
+      'body-max-line-length': [0, 'always', 100],
+    },
+  };
+  
