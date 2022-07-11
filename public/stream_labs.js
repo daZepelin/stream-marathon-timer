faaed47d96ee69fc64f791ff7a6cccc1536e7dbc
Vytautas Serelis <vytutas2@gmail.com>
2022-07-11 15:17:20 +0300

    tweak: added fb stars handler

--- public/stream_labs.js
+++ public/stream_labs.js
@@ -4,8 +4,8 @@ var streamlabsSocket = null;
 const initLabs = (authToken) => {
     streamlabsSocket = io(`https://sockets.streamlabs.com?token=${authToken}`, { transports: ['websocket'] });
     streamlabsSocket.on('event', (eventData) => {
+        console.log(eventData)
         if (eventData.type === 'donation') {
-            console.log(eventData.message[0])
             var currency = eventData.message[0]?.formatted_amount?.charAt(0);
             if (currency == '$' || currency == '€' || currency == '£') {
                 countDownDate = new Date(
@@ -17,6 +17,15 @@ const initLabs = (authToken) => {
                 var distance = countDownDate.getTime() - now;
                 $.post('http://localhost:3000/setTimer', { time: Math.floor(distance / 1000) });
             }
+        } else if (eventData.type === 'stars' ) {
+            countDownDate = new Date(
+                countDownDate.getTime() + eventData.message[0].amount * timeMultiplier * 600
+            );
+
+            var now = new Date().getTime();
+
+            var distance = countDownDate.getTime() - now;
+            $.post('http://localhost:3000/setTimer', { time: Math.floor(distance / 1000) });
         }
     });
 };
