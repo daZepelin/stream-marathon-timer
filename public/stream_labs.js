8fc39ffef1adf68b6b5ac65ce6408b4b094486cb
Vytautas Serelis <vytutas2@gmail.com>
2022-07-11 14:45:31 +0300

    tweak: added currency whitelist for stream labs

--- public/stream_labs.js
+++ public/stream_labs.js
@@ -4,15 +4,19 @@ var streamlabsSocket = null;
 const initLabs = (authToken) => {
     streamlabsSocket = io(`https://sockets.streamlabs.com?token=${authToken}`, { transports: ['websocket'] });
     streamlabsSocket.on('event', (eventData) => {
-        console.log('event')
         if (eventData.type === 'donation') {
-            console.log(eventData.message[0].amount);
-            countDownDate = new Date(countDownDate.getTime() + eventData.message[0].amount * timeMultiplier * 60000);
+            console.log(eventData.message[0])
+            var currency = eventData.message[0]?.formatted_amount?.charAt(0);
+            if (currency == '$' || currency == '€' || currency == '£') {
+                countDownDate = new Date(
+                    countDownDate.getTime() + eventData.message[0].amount * timeMultiplier * 60000
+                );
 
-            var now = new Date().getTime();
+                var now = new Date().getTime();
 
-            var distance = countDownDate.getTime() - now;
-            $.post('http://localhost:3000/setTimer', { time: Math.floor(distance / 1000) });
+                var distance = countDownDate.getTime() - now;
+                $.post('http://localhost:3000/setTimer', { time: Math.floor(distance / 1000) });
+            }
         }
     });
 };
