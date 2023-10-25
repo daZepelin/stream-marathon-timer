03ee5cb61d35a0d7b03b804d39f40cda0a014ea9
Vytautas Serelis <vytutas2@gmail.com>
2023-10-25 21:30:20 +0300

    refactor: restructured to tauri

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'chore',
-          'build',
-          'ci',
-          'docs',
-          'feat',
-          'fix',
-          'perf',
-          'refactor',
-          'revert',
-          'style',
-          'tweak',
-        ],
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'build',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'tweak',
       ],
-      'body-max-line-length': [0, 'always', 100],
-    },
-  };
-  
+    ],
+    'body-max-line-length': [0, 'always', 100],
+  },
+};
