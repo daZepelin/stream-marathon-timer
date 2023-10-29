385c36e6e7b1efdb5388549ba75289c80fdbf4b4
Vytautas Serelis <vytutas2@gmail.com>
2023-10-29 14:10:24 +0200

    tweak(fe/ui): navbar links

--- src/routes/dashboard/navbar/Navbar.tsx
+++ src/routes/dashboard/navbar/Navbar.tsx
@@ -1,44 +1,31 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Group, Code } from '@mantine/core';
-import {
-  IconBellRinging,
-  IconFingerprint,
-  IconKey,
-  IconSettings,
-  Icon2fa,
-  IconDatabaseImport,
-  IconReceipt2,
-  IconSwitchHorizontal,
-  IconLogout,
-} from '@tabler/icons-react';
+import { IconPokerChip, IconHourglass, IconRefresh, IconX } from '@tabler/icons-react';
 import { MantineLogo } from '@mantine/ds';
 import classes from './Navbar.module.css';
-
-
+import { useNavigate } from 'react-router-dom';
 
 const data = [
-  { link: '', label: 'Notifications', icon: IconBellRinging },
-  { link: '', label: 'Billing', icon: IconReceipt2 },
-  { link: '', label: 'Security', icon: IconFingerprint },
-  { link: '', label: 'SSH Keys', icon: IconKey },
-  { link: '', label: 'Databases', icon: IconDatabaseImport },
-  { link: '', label: 'Authentication', icon: Icon2fa },
-  { link: '', label: 'Other Settings', icon: IconSettings },
+  // { link: '/dashboard/subathon-timer', label: 'Dashboard', icon: IconLayoutCollage },
+  { link: '/dashboard/subathon-timer', label: 'Subathon', icon: IconHourglass },
+  { link: '/dashboard/lucky-wheel', label: 'Lucky wheel', icon: IconPokerChip, disabled: true },
 ];
 
 export function Navbar() {
-  const [active, setActive] = useState('Billing');
+  const [active, setActive] = useState('Subathon');
+  const navigate = useNavigate();
   const links = data.map((item) => (
     <a
       className={classes.link}
       data-active={item.label === active || undefined}
+      data-disabled={item.disabled || undefined}
       href={item.link}
       key={item.label}
       onClick={(event) => {
         event.preventDefault();
         setActive(item.label);
-      }}
-    >
+        navigate(item.link);
+      }}>
       <item.icon className={classes.linkIcon} stroke={1.5} />
       <span>{item.label}</span>
     </a>
@@ -47,7 +34,7 @@ export function Navbar() {
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbarMain}>
-        <Group className={classes.header} justify="space-between">
+        <Group className={classes.header} justify='space-between'>
           <MantineLogo size={28} />
           <Code fw={700}>v3.1.2</Code>
         </Group>
@@ -55,14 +42,14 @@ export function Navbar() {
       </div>
 
       <div className={classes.footer}>
-        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
-          <span>Change account</span>
+        <a href='#' className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconRefresh className={classes.linkIcon} stroke={1.5} />
+          <span>Refresh</span>
         </a>
 
-        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
-          <IconLogout className={classes.linkIcon} stroke={1.5} />
-          <span>Logout</span>
+        <a href='#' className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconX className={classes.linkIcon} stroke={1.5} />
+          <span>Close</span>
         </a>
       </div>
     </nav>
