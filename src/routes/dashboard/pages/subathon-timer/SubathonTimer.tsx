5b446b4a1707db59dd4280f7cd9bc840f1aba519
Vytautas Serelis <vytutas2@gmail.com>
2023-10-31 01:10:20 +0200

    feat(fe/ui): obs source link

--- src/routes/dashboard/pages/subathon-timer/SubathonTimer.tsx
+++ src/routes/dashboard/pages/subathon-timer/SubathonTimer.tsx
@@ -1,14 +1,33 @@
-import { Grid, Paper, Title, useMantineTheme } from '@mantine/core';
+import { ActionIcon, CopyButton, Grid, Input, Paper, Title, Tooltip, rem, useMantineTheme } from '@mantine/core';
 import Settings from './components/settings/Settings';
 import { formatTime } from '../../../../services/utils';
 import useSubathonTime from '../../../../hooks/useSubathonTime';
 import useSubathonTimerConfig from '../../../../hooks/useSubathonTimerConfig';
 import DonationLogs from './components/donation-logs/DonationLogs';
+import { useEffect, useState } from 'react';
+import { IconCheck, IconCopy } from '@tabler/icons-react';
 
 export const SubathonTimer = () => {
   const { subathonTime } = useSubathonTime();
   const { subathonTimerStyle } = useSubathonTimerConfig();
+  const [displayStyle, setDisplayStyle] = useState<boolean>(false);
+  const [displayTimeout, setDisplayTimeout] = useState<number | null>(null);
   const theme = useMantineTheme();
+
+  useEffect(() => {
+    if (displayTimeout) clearTimeout(displayTimeout);
+    setDisplayStyle(true);
+    let timeout = setTimeout(() => {
+      setDisplayStyle(false);
+    }, 3000);
+
+    setDisplayTimeout(timeout);
+
+    return () => {
+      if (displayTimeout) clearTimeout(displayTimeout);
+    };
+  }, [subathonTimerStyle]);
+
   return (
     <Grid grow align='flex-start'>
       <Grid.Col span={12}>
@@ -22,8 +41,9 @@ export const SubathonTimer = () => {
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
-            justifyContent: 'space-between',
+            justifyContent: 'flex-start',
             overflow: 'hidden',
+            gap: 10,
           }}>
           <Title
             order={2}
@@ -33,16 +53,40 @@ export const SubathonTimer = () => {
             }}>
             Subathon Timer
           </Title>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <Input.Wrapper size='xs' description='OBS source link'>
+              <Input
+                size='xs'
+                rightSectionPointerEvents='all'
+                value={'http://localhost:1427/subathon-timer'}
+                rightSection={
+                  <CopyButton value='http://localhost:1427/subathon-timer' timeout={2000}>
+                    {({ copied, copy }) => (
+                      <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow position='right'>
+                        <ActionIcon color={copied ? 'teal' : 'gray'} variant='subtle' onClick={copy}>
+                          {copied ? <IconCheck style={{ width: rem(16) }} /> : <IconCopy style={{ width: rem(16) }} />}
+                        </ActionIcon>
+                      </Tooltip>
+                    )}
+                  </CopyButton>
+                }
+              />
+            </Input.Wrapper>
+          </div>
           <div
             style={{
               height: '57px',
               overflow: 'hidden',
+              position: 'absolute',
+              right: 30,
             }}>
             <div
               style={{
                 lineHeight: '57px',
-                ...subathonTimerStyle,
-                textShadow: ` ${subathonTimerStyle['font-border-width']}px 0 0 ${subathonTimerStyle['font-border-color']},  ${subathonTimerStyle['font-border-width']}px 0 0 ${subathonTimerStyle['font-border-color']},  ${subathonTimerStyle['font-border-width']}px 0 0 ${subathonTimerStyle['font-border-color']},  ${subathonTimerStyle['font-border-color']} ${subathonTimerStyle['font-border-color']} 0 0`,
+                ...(displayStyle ? subathonTimerStyle : {}),
+                textShadow: displayStyle
+                  ? `${subathonTimerStyle['font-border-width']}px 0 0 ${subathonTimerStyle['font-border-color']},  ${subathonTimerStyle['font-border-width']}px 0 0 ${subathonTimerStyle['font-border-color']},  ${subathonTimerStyle['font-border-width']}px 0 0 ${subathonTimerStyle['font-border-color']},  ${subathonTimerStyle['font-border-color']} ${subathonTimerStyle['font-border-color']} 0 0`
+                  : '',
               }}>
               {formatTime(subathonTime)}
             </div>
@@ -52,7 +96,7 @@ export const SubathonTimer = () => {
       <Grid.Col span={6}>
         <Settings />
       </Grid.Col>
-      <Grid.Col span={6} >
+      <Grid.Col span={6}>
         <DonationLogs />
       </Grid.Col>
     </Grid>
